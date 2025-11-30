fc0d25ab55c0a004f6ded41fb75b91a952df2e05
mahdi noori <mahdi.n@mahdis-MacBook-Pro.local>
2025-11-30 17:25:42 +0330

    add order_resend,order_edit,order_detail,order_deadline,order_cancell page

--- order/views.py
+++ order/views.py
@@ -20,3 +20,20 @@ def fail_payment(request):
 
 def return_procedure(request):
     return render(request, "order/return-procedure.html") 
+
+
+
+def order_resend(request):
+    return render(request, "account/orders/seller-panel-order-resend.html") 
+
+def order_edit(request):
+    return render(request, "account/orders/seller-panel-order-edit.html") 
+
+def order_detail(request):
+    return render(request, "account/orders/seller-panel-order-detail.html") 
+
+def order_deadline(request):
+    return render(request, "account/orders/seller-panel-order-deadline.html") 
+
+def order_cancell(request):
+    return render(request, "account/orders/seller-panel-order-cancell.html") 
